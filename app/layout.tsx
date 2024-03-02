90f21aff3e46afe2332c2da63e13ed9400627c46
Betty Andersson <betina1995@hotmail.com>
2024-03-02 17:07:50 +0100

    fix: try to fix hydration issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7644729638272332"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body className={inter.className}>
+        {children}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7644729638272332"
+          crossOrigin="anonymous"
+        ></script>
+        <ins
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-7644729638272332"
+          data-ad-slot="5413643973"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </body>
     </html>
   );
 }
