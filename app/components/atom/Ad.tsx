1fa816959508581e40e4ef4cc94a79286463bdab
Betty Andersson <betina1995@hotmail.com>
2024-03-02 10:31:30 +0100

    feat: try to add an ad

---
+++ app/components/atom/Ad.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const BettysAd: React.FC = () => {
+  return <div>This is an ad</div>;
+};
+
+export default BettysAd;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import BettysAd from "./components/atom/Ad";
 
 export default function Home() {
-  return <main className={styles.main}>Welcome to DateSplit!</main>;
+  return (
+    <main className={styles.main}>
+      Welcome to DateSplit!
+      <BettysAd />
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-7644729638272332"
+        data-ad-slot="5413643973"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7644729638272332"
+        crossOrigin="anonymous"
+      ></script>
+    </main>
+  );
 }
